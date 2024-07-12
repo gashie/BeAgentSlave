6428b5ef30461d194e2cd490191687a468bfbd25
gashie <richardwilliam60@gmail.com>
2024-07-12 11:49:54 +0000

    complete pm2

--- controllers/pm2/manage.js
+++ controllers/pm2/manage.js
@@ -1,7 +1,7 @@
 const pm2 = require('pm2');
 const asynHandler = require("../../middleware/async");
 const { sendResponse, CatchHistory } = require("../../helper/utilfunc");
-const { startProcess,listProcesses,stopProcess } = require('../../helper/pm2');
+const { startProcess,listProcesses,stopProcess,getLogs,restartProcess,deleteProcess,describeProcess } = require('../../helper/pm2');
 const systemDate = new Date().toISOString().slice(0, 19).replace("T", " ");
 
 exports.ManagePm2 = asynHandler(async (req, res, next) => {
@@ -14,22 +14,47 @@ pm2.connect(async(err) => {
     }
 
     try {
+
+
         switch (action) {
-          case 'start':
-            await startProcess(processName);
-            res.json({ message: `Started PM2 process: ${processName}` });
-            break;
-          case 'stop':
-            await stopProcess(processName);
-            res.json({ message: `Stopped PM2 process: ${processName}` });
-            break;
-          case 'list':
-            const processes = await listProcesses();
-            res.json({ processes });
-            break;
-          default:
-            res.status(400).json({ error: 'Invalid action' });
-        }
+            case 'start':
+              await startProcess(scriptPath, options);
+            //   ws.send(JSON.stringify({ message: `Started PM2 process: ${processName}` }));
+              return sendResponse(res, 1, 200, `Started PM2 process: ${processName}`, []);
+              break;
+            case 'stop':
+              await stopProcess(processName);
+            //   ws.send(JSON.stringify({ message: `Stopped PM2 process: ${processName}` }));
+              return sendResponse(res, 1, 200, `Stopped PM2 process: ${processName}`, []);
+
+              break;
+            case 'list':
+              const processes = await listProcesses();
+            //   ws.send(JSON.stringify({ processes }));
+              return sendResponse(res, 1, 200, `Process list retrieved`, processes);
+
+              break;
+            case 'restart':
+              await restartProcess(processName);
+              return sendResponse(res, 1, 200, `Restarted PM2 process: ${processName}`, []);
+            //   ws.send(JSON.stringify({ message: `Restarted PM2 process: ${processName}` }));
+              break;
+            case 'delete':
+              await deleteProcess(processName);
+              return sendResponse(res, 1, 200, `Deleted PM2 process: ${processName}`, []);
+            //   ws.send(JSON.stringify({ message: `Deleted PM2 process: ${processName}` }));
+              break;
+            case 'describe':
+              const description = await describeProcess(processName);
+              ws.send(JSON.stringify({ description }));
+              break;
+            case 'logs':
+              const logs = await getLogs(processName);
+              ws.send(JSON.stringify({ logs }));
+              break;
+            default:
+                res.status(400).json({ error: 'Invalid action' });
+            }
       } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to perform PM2 action' });
--- helper/pm2.js
+++ helper/pm2.js
@@ -1,32 +1,5 @@
 const pm2 = require('pm2');
 
-// Function to start a PM2 process
-const startProcess = (processName) => {
-  return new Promise((resolve, reject) => {
-    pm2.start(processName, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-};
-
-// Function to stop a PM2 process
-const stopProcess = (processName) => {
-  return new Promise((resolve, reject) => {
-    pm2.stop(processName, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-};
-
-// Function to list PM2 processes
 const listProcesses = () => {
   return new Promise((resolve, reject) => {
     pm2.list((err, processes) => {
@@ -39,9 +12,85 @@ const listProcesses = () => {
   });
 };
 
+const startProcess = (scriptPath, options = {}) => {
+  return new Promise((resolve, reject) => {
+    pm2.start(scriptPath, options, (err, proc) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(proc);
+      }
+    });
+  });
+};
+
+const stopProcess = (processName) => {
+  return new Promise((resolve, reject) => {
+    pm2.stop(processName, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ message: `Stopped PM2 process: ${processName}` });
+      }
+    });
+  });
+};
+
+const restartProcess = (processName) => {
+  return new Promise((resolve, reject) => {
+    pm2.restart(processName, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ message: `Restarted PM2 process: ${processName}` });
+      }
+    });
+  });
+};
+
+const deleteProcess = (processName) => {
+  return new Promise((resolve, reject) => {
+    pm2.delete(processName, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ message: `Deleted PM2 process: ${processName}` });
+      }
+    });
+  });
+};
+
+const describeProcess = (processName) => {
+  return new Promise((resolve, reject) => {
+    pm2.describe(processName, (err, description) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(description);
+      }
+    });
+  });
+};
+
+const getLogs = (processName) => {
+  return new Promise((resolve, reject) => {
+    pm2.logs(processName, (err, logs) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(logs);
+      }
+    });
+  });
+};
+
 // Exporting the functions
 module.exports = {
   startProcess,
   stopProcess,
   listProcesses,
+  restartProcess,
+  deleteProcess,
+  describeProcess,
+  getLogs
 };
